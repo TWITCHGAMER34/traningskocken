47169a302a81f6bc432b8800fdc2ef4da523f156
blahblahblah11111 <robotskylander@gmail.com>
2024-09-02 14:07:51 +0200

    Payment button

--- src/Pages/Medlem/index.tsx
+++ src/Pages/Medlem/index.tsx
@@ -17,27 +17,37 @@ export default function bliMedlem() {
         <div className='all_options'>
 
         <div className='left_box'>
-            <h2>Prisgrup 1</h2>
+            <h2>Prisgrupp 1</h2>
         <ul className='lists'>
 
             <li>Permanent rabbat på 5%!</li>
             <li>Notifikationer när nya rätter adderas till menyn.</li>
         
          </ul>
+
+         <div className='payment_button'>
+            <p>X Kr/Månad</p>
+         </div>
+
         </div>
 
         <div className='middle_box'>
-        <h2>Prisgrup 2</h2>
+        <h2>Prisgrupp 2</h2>
         <ul className='lists'>
             <li>Permanent rabbat på 5%!</li>
             <li>Notifikationer när nya rätter adderas till menyn.</li>
             <li>Gratis leverans!</li>
 
          </ul>
+
+         <div className='payment_button'>
+            <p>X Kr/Månad</p>
+         </div>
+
         </div>
 
         <div className='right_box'>
-        <h2>Prisgrup 3</h2>
+        <h2>Prisgrupp 3</h2>
         <ul className='lists'>
 
             <li>Permanent rabbat på 5%!</li>
@@ -46,11 +56,17 @@ export default function bliMedlem() {
             <li>En professionel kock rekommenderar mat till dig baserat på dina krav.</li>
 
          </ul>
+
+         <div className='payment_button'>
+            <p>X Kr/Månad</p>
+         </div>
+         
         </div>
 
         </div>
-
+        <div className='bottom_text'>
         <p>Genom att bli medlem hjälper du oss och gör livet lättare för dig.</p>
+        </div>
         </>
 
 
