127010f2f1c8d5c50bd5ce64b8e503c5424c821c
blahblahblah11111 <robotskylander@gmail.com>
2024-09-04 11:20:45 +0200

    for sync

---
+++ src/Pages/meny/index.tsx
@@ -0,0 +1,13 @@
+import {Helmet} from 'react-helmet';
+import "./menu.scss";
+
+export default function Menu(){
+    return(
+        <>
+        <Helmet>MENY</Helmet>
+    
+        <h1>shvcshdbcjbskjdbc</h1>
+    </>
+    )
+
+}
