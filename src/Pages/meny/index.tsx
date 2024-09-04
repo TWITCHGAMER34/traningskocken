70ec1ddccb64a69196e217c3589f0ed352ac55e6
blahblahblah11111 <robotskylander@gmail.com>
2024-09-04 14:48:23 +0200

    menu

--- src/Pages/meny/index.tsx
+++ src/Pages/meny/index.tsx
@@ -6,12 +6,34 @@ export default function Menu(){
         <>
         <Helmet>MENY</Helmet>
     
-        <div className='training_selection'>
-            <p>Välj den meny som passar din träning</p>
-            <button>Styrke Träning</button>
-            <button>Konditions Träning</button>
-            <button>HIT</button>
+            
+        <div class="container">
 
+             <div class="Buttons">
+                <button>Styrke Träning</button>
+                <button>Konditions Träning</button>
+                <button>HTI</button>
+            </div>
+
+
+            <div class="item_1"> 
+                <img src="burger.jpeg" width="300" height="300"></img> 
+                <h2>Mat Namn</h2>
+                <p>Pris: x Kr</p>
+                <p>Kalorier: x</p>
+                
+
+            </div>
+
+            <div class="item_2"> <img src="burger.jpeg" width="300" height="300"></img> </div>
+
+            <div class="item_3"> <img src="burger.jpeg" width="300" height="300"></img> </div>
+
+            <div class="item_4"> <img src="burger.jpeg" width="300" height="300"></img> </div>
+
+            <div class="item_5"> <img src="burger.jpeg" width="300" height="300"></img> </div>
+
+            <div class="item_6"> <img src="burger.jpeg" width="300" height="300"></img> </div>
         </div>
     </>
     )
