b812a7c2e00153f84d4f73e95ae1d99e2274c824
blahblahblah11111 <robotskylander@gmail.com>
2024-09-04 10:07:13 +0200

    done

--- src/Pages/Medlem/option.tsx
+++ src/Pages/Medlem/option.tsx
@@ -1,10 +1,12 @@
+import "./medlem.scss";
+
 export function Option({title, items, price}: { title: string, items: string[], price: string }) {
     return <div className='box'>
         <h2>{title}</h2>
         <ul className='lists'>
             {items.map((item, i) => <li key={item + i}>{item}</li>)}
         </ul>
-        <p>{price} Kr/Månad</p>
-        <button>Prenumerera</button>
+        <b>{price} Kr/Månad</b>
+        <button className="payment_button">Prenumerera</button>
     </div>
 }
